c1f6debad790f85b739ce283ab6b5a8d5bdbf8e5
alexandreleao <erdleosalgueiro@gmail.com>
2021-11-19 10:26:40 -0300

    comit inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+       <form action="action.php" method="podt">
+           <p>Your name: <input type="text" name="name"/></p>
+           <p>Your <input type="text" name="age"/></p>
+           <p><input type="submit" /></p>
+       </form>
+    </body>
+</html>
